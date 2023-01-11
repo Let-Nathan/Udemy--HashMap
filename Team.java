729d85d1fac4282a392a565eb6856fe2059c44d2
Nathan <letournel.nathan@gmail.com>
2023-01-11 14:52:09 +0100

    Udemy HashMap

---
+++ Team.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Team {
+
+    private String name;
+    private Map<String, String> players;
+
+    public Team(String name) {
+       this.name = name;
+       this.players = new HashMap<>();
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayer(String position) {
+        return players.get(position);
+    }
+
+    public void setPlayer(String position, String player) {
+        players.put(position, player);
+    }    
+
+    
+}
